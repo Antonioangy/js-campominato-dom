927a597eac97c0a82b8cb0d5e89366867e8f29ae
Antonioangy <dumitruangy26@gmail.com>
2021-11-03 18:49:36 +0100

    js

--- js/script.js
+++ js/script.js
@@ -25,18 +25,51 @@ if (numeroUtente == 3) {
     nome = 'difficile';
 }
 
+
+//Array con i numeri randomici delle caselle con bomba
+let bombe = []; 
+for (var i = 0; i < 16; i++){
+    //Estrazione casuale di num compreso tra 0 e la difficolta
+    var numBomba =Math.floor(Math.random()*difficult)
+    //inserisco il numero estratto nell'array
+    console.log( numBomba);
+    bombe.push(numBomba)
+}
+
 // creo un ciclo for per la difficolta' 1
 
+
 for (let i = 0; i < difficult; i++) {
-    console.log(i);
+    //console.log(i);
     let nodo = document.createElement('div');
     nodo.addEventListener("click", function(){
-        nodo.classList.add('click');
+        if(bombe.includes(i)){
+            nodo.classList.add('bomba');
+        } else {
+            nodo.classList.add('click');
+        }
+        
     })
     nodo.classList.add('square');
     nodo.classList.add(nome);
     nodo.innerText = i;
     document.querySelector('#grid').append(nodo);
 }
+
+//per il punteggio
+
+let punteggio = 0;
+
+document.getElementById('punteggio').innerHTML = punteggio;
+
+var elements = document.getElementsByClassName("square");
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function(){
+        punteggio = punteggio + 1;
+        document.getElementById('punteggio').innerHTML = punteggio;
+    });
+}
+
 
 
